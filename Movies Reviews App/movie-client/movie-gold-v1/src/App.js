6ec4d1739ce24475df7d0907410a366dd3a0373e
Matei Otniel-Daniel <matei.otniel20@gmail.com>
2023-02-15 00:23:41 +0200

    Why doesn't it work!

--- Movies Reviews App/movie-client/movie-gold-v1/src/App.js
+++ Movies Reviews App/movie-client/movie-gold-v1/src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
-import api from './api/axiosConfig'
+import api from "./api/axiosConfig";
 import {useState, useEffect} from 'react';
+import userEvent from '@testing-library/user-event';
 
 function App() {
 
   const [movies, setMovies] = useState();
-  
-  const getMovies = async () =>{
+
+  const getMovies = async () => {
+    
     try{
       const response = await api.get("/api/v1/movies");
-      console.log(response.data);
+      
+      console.log(response.data)
+
       setMovies(response.data);
-    } catch (err){
+
+    } catch(err){
       console.log(err);
     }
-
   }
 
   useEffect(() => {
